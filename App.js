300811ac18a981e2fa67abc9c4756d52fb5834a6
Ichlasul Amal <ichlasul0899@gmail.com>
2023-10-17 22:41:46 +0700

    Create image and button

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable  } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
+const placeHolderImage = require('./assets/images/background-image.png');
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to learn react native</Text>
+      <Text style={styles.title}>Welcome Bro</Text>
+      <Image source={placeHolderImage} style={styles.image}  />
+
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button} onPress={() => console.log('Anda akan login.')}>
+          <FontAwesome
+            name="sign-in"
+            size={18}
+            color="white"
+            style={styles.buttonIcon}
+          />
+          <Text style={styles.buttonLabel}>LOGIN</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +31,41 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title:{
+    fontSize:20,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+    backgroundColor: '#green',
+    borderRadius: 35,
+    marginTop: 20
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TopFin
 Created with expo go
+
+Learning : 
+000 - Init app
+001 - Create Image
+002 - Create Button and Title
+
